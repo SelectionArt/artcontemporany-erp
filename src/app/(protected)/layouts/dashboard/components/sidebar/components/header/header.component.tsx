5cc6809e52e23f07ea21ca41118145e2310aa03d
SelectionArt <info@artcontemporany.com>
2025-02-14 13:36:27 +0100

    updates

--- src/app/(protected)/layouts/dashboard/components/sidebar/components/header/header.component.tsx
+++ src/app/(protected)/layouts/dashboard/components/sidebar/components/header/header.component.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 // Icons
-import { GalleryVerticalEnd } from "lucide-react";
+import { Brush } from "lucide-react";
 
 const Header = () => {
   return (
-    <div>
-      <GalleryVerticalEnd />
-      <span>Logo</span>
+    <div className="flex gap-2 p-4">
+      <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+        <Brush className="size-4" />
+      </div>
+      <div className="grid flex-1 text-left text-sm leading-tight">
+        <span className="truncate font-semibold">Art Contemporany</span>
+        <span className="truncate text-xs">Administrator</span>
+      </div>
     </div>
   );
 };
