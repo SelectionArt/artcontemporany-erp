43e83c2d3bebd3d087b0d38e24bfbb8bdda62feb
SelectionArt <info@artcontemporany.com>
2025-02-16 00:09:01 +0100

    updates

--- src/app/(protected)/(pages)/galeria/components/filter/filter.component.tsx
+++ src/app/(protected)/(pages)/galeria/components/filter/filter.component.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Check, PlusCircle } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Command,
