6d03045a6eb888990c6ca5dbe2de113fe5af21ca
SelectionArt <info@artcontemporany.com>
2025-02-25 12:15:45 +0100

    updates

--- src/app/(protected)/layouts/dashboard/actions/dashboard.actions.ts
+++ src/app/(protected)/layouts/dashboard/actions/dashboard.actions.ts
@@ -6,14 +6,15 @@ import type { FetchPricingsReturn } from "./types/dashboard.actions.types";
 
 const fetchPricings = async (): Promise<FetchPricingsReturn> => {
   try {
-    const sections = await prisma.pricing.findMany({
+    const pricings = await prisma.pricing.findMany({
       select: {
         id: true,
         name: true,
+        type: true,
       },
       orderBy: { name: "asc" },
     });
-    return sections;
+    return pricings;
   } catch (error) {
     console.error(error);
     return [];
